1c212395598bebc48672cc596199e3924b025b99
mateusz <m.dzieciatkowski@gmail.com>
2025-08-05 23:06:38 +0200

    Initial commit

---
+++ create_db_tables.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+cursor = sqlite3.connect('weather.db')
+
+cursor.execute("""
+    CREATE TABLE city (
+	id	INTEGER,
+	name	TEXT UNIQUE,
+	country	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT)
+	)
+    """)
+
+cursor.execute("""
+    CREATE TABLE weather(
+               id INTEGER,
+               city_id INTEGER,
+               date TEXT,
+               temperature NUMERIC,
+               humidity NUMERIC,
+               wind_speed NUMERIC,
+               wind_direction NUMERIC,
+               PRIMARY KEY("id" AUTOINCREMENT),
+               FOREIGN KEY (city_id) REFERENCES city(id)
+    )
+    """)
+
+cursor.commit()
+cursor.close()
